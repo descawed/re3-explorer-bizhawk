63fa22ea779f14d4f52371b6f0ee2391bf266f4d
descawed <github@descawed.com>
2024-10-31 20:18:50 -0400

    Preliminary call tracking

--- src/GameVersion.cs
+++ src/GameVersion.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+using System.IO;
 using BizHawk.Client.Common;
 
 namespace Re3Explorer;
 
 public abstract class GameVersion(IMemoryApi memory) {
-    protected IMemoryApi Memory { get; } = memory;
+    private IMemoryApi Memory { get; } = memory;
     protected abstract byte[] RandTrackerPatch { get; }
     protected abstract uint RandTrackerPatchAddress { get; }
     protected abstract uint RandTrackerDataAddress { get; }
@@ -16,6 +18,8 @@ public abstract class GameVersion(IMemoryApi memory) {
     protected abstract uint ScriptRandAddress { get; }
     protected abstract uint RoomNumberAddress { get; }
     protected abstract uint StageNumberAddress { get; }
+
+    private uint _randTrackingOverwriteValue;
 
     public string RoomId {
         get {
@@ -32,4 +36,39 @@ public abstract class GameVersion(IMemoryApi memory) {
     public byte ScriptRngOffset => (byte)Memory.ReadU8(RandOffsetsAddress + ScriptRngOffsetIndex);
 
     public ushort ScriptRngState => (ushort)Memory.ReadU16(ScriptRandValueAddress);
+
+    public void ApplyRandTrackingPatch() {
+        _randTrackingOverwriteValue = ApplyTrackingPatch(RandFunctionAddress, RandTrackerPatchAddress, RandTrackerPatch, RandTrackerDataAddress, RandTrackerDataSize);
+    }
+
+    public IList<uint> GetRandCalls() {
+        var callers = new List<uint>();
+
+        var dataEnd = RandTrackerDataAddress + RandTrackerDataSize;
+        if (Memory.ReadU32(dataEnd) != _randTrackingOverwriteValue) {
+            throw new InvalidDataException("Rand call tracker overflowed!");
+        }
+        
+        for (var address = RandTrackerDataAddress; address < dataEnd; address += 4) {
+            var caller = Memory.ReadU32(address);
+            if (caller == 0) {
+                break;
+            }
+            
+            callers.Add(caller);
+        }
+
+        // reset data block
+        Memory.WriteByteRange(RandTrackerDataAddress, new byte[RandTrackerDataSize]);
+        
+        return callers;
+    }
+
+    private uint ApplyTrackingPatch(uint jumpAddress, uint patchAddress, byte[] patch, uint dataAddress, uint dataSize) {
+        Memory.WriteU32(jumpAddress, 0x08000000 | (patchAddress >> 2));
+        Memory.WriteByteRange(patchAddress, patch);
+        Memory.WriteByteRange(dataAddress, new byte[dataSize]);
+        // return the value past the end of the data block for detection of out of bounds writes
+        return Memory.ReadU32(dataAddress + dataSize);
+    }
 }
--- src/Re3Explorer.cs
+++ src/Re3Explorer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 using BizHawk.Client.Common;
@@ -12,6 +13,16 @@ namespace Re3Explorer;
 // currently only SLPM-87224 supported
 [ExternalToolApplicability.SingleRom(VSystemID.Raw.PSX, GameVersionSlpm87224.Hash)]
 public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
+    private struct CallStats {
+        public uint FrameCalls = 0;
+        public uint RoomCalls = 0;
+        public uint TotalCalls = 0;
+
+        public CallStats() { }
+    }
+    
+    private const ushort InitialRngState = 0x6CA4;
+
     public ApiContainer? MaybeApi { get; set; }
 
     private ApiContainer Api => MaybeApi!;
@@ -20,8 +31,18 @@ public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
 
     private GameVersion? _version;
 
+    private CallStats _randCalls;
+    private CallStats _scriptCalls;
+
+    private bool _trackingPatchesApplied;
+    private string _lastRoom = "";
+    private bool _framerateToggle;
+
     private readonly Label _roomLabel;
     private readonly Label _rngValueLabel;
+    private readonly Label _rngFrameCallsLabel;
+    private readonly Label _rngRoomCallsLabel;
+    private readonly Label _rngTotalCallsLabel;
     private readonly Label _scriptRngValueLabel;
     private readonly Label _scriptRngOffsetIndexLabel;
     private readonly Label _scriptRngOffsetLabel;
@@ -60,6 +81,12 @@ public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
         table.Controls.Add(new Label { Anchor = AnchorStyles.Right, AutoSize = true, Font = font, Text = "RNG" }, 0, 1);
         _rngValueLabel = new Label { AutoSize = true, Font = font, Text = "" };
         table.Controls.Add(_rngValueLabel, 1, 1);
+        _rngFrameCallsLabel = new Label { AutoSize = true, Font = font, Text = "" };
+        table.Controls.Add(_rngFrameCallsLabel, 2, 1);
+        _rngRoomCallsLabel = new Label { AutoSize = true, Font = font, Text = "" };
+        table.Controls.Add(_rngRoomCallsLabel, 3, 1);
+        _rngTotalCallsLabel = new Label { AutoSize = true, Font = font, Text = "" };
+        table.Controls.Add(_rngTotalCallsLabel, 4, 1);
         
         // script RNG
         table.Controls.Add(new Label { Anchor = AnchorStyles.Right, AutoSize = true, Font = font, Text = "Script RNG" }, 0, 2);
@@ -69,10 +96,16 @@ public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
         table.Controls.Add(new Label { Anchor = AnchorStyles.Right, AutoSize = true, Font = font, Text = "Script RNG offset index" }, 0, 3);
         _scriptRngOffsetIndexLabel = new Label { AutoSize = true, Font = font, Text = "" };
         table.Controls.Add(_scriptRngOffsetIndexLabel, 1, 3);
+        table.Controls.Add(new Label { AutoSize = true, Font = font, Text = "N/A" }, 2, 3);
+        table.Controls.Add(new Label { AutoSize = true, Font = font, Text = "N/A" }, 3, 3);
+        table.Controls.Add(new Label { AutoSize = true, Font = font, Text = "N/A" }, 4, 3);
         
         table.Controls.Add(new Label { Anchor = AnchorStyles.Right, AutoSize = true, Font = font, Text = "Script RNG offset" }, 0, 4);
         _scriptRngOffsetLabel = new Label { AutoSize = true, Font = font, Text = "" };
         table.Controls.Add(_scriptRngOffsetLabel, 1, 4);
+        table.Controls.Add(new Label { AutoSize = true, Font = font, Text = "N/A" }, 2, 4);
+        table.Controls.Add(new Label { AutoSize = true, Font = font, Text = "N/A" }, 3, 4);
+        table.Controls.Add(new Label { AutoSize = true, Font = font, Text = "N/A" }, 4, 4);
         
         root.Controls.Add(_roomLabel);
         root.Controls.Add(table);
@@ -89,6 +122,18 @@ public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
         Left = parentPosition.X + Owner.Width;
     }
 
+    private void UpdateStats() {
+        if (_version is null || !_trackingPatchesApplied) {
+            return;
+        }
+
+        var randCalls = _version.GetRandCalls();
+        var numRandCalls = (uint)randCalls.Count;
+        _randCalls.FrameCalls = numRandCalls;
+        _randCalls.RoomCalls += numRandCalls;
+        _randCalls.TotalCalls += numRandCalls;
+    }
+
     public override void Restart() {
         // TODO: reset stats and register event listeners
         if (Api.Emulation.GetGameInfo() is { } gameInfo) {
@@ -96,14 +141,33 @@ public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
         } else {
             _version = null;
         }
+
+        _trackingPatchesApplied = false;
+        _lastRoom = "";
+        _randCalls = new();
+        _scriptCalls = new();
     }
 
     protected override void UpdateAfter() {
         if (_version is null) {
             return;
         }
+
+        var currentRoom = _version.RoomId;
+        if (currentRoom != _lastRoom) {
+            _lastRoom = currentRoom;
+            _randCalls.RoomCalls = 0;
+            _scriptCalls.RoomCalls = 0;
+        }
+
+        if (_trackingPatchesApplied) {
+            UpdateStats();
+        } else if (_version.RngState == InitialRngState) {
+            _version.ApplyRandTrackingPatch();
+            _trackingPatchesApplied = true;
+        }
         
-        _roomLabel.Text = $"Room: {_version.RoomId}";
+        _roomLabel.Text = $"Room: {currentRoom}";
         _rngValueLabel.Text = $"{_version.RngState:X04}";
         _scriptRngValueLabel.Text = $"{_version.ScriptRngState:X04}";
         
@@ -111,5 +175,16 @@ public sealed class Re3Explorer: ToolFormBase, IExternalToolForm {
         var scriptRngOffset = _version.ScriptRngOffset;
         _scriptRngOffsetIndexLabel.Text = $"{scriptRngOffsetIndex} ({scriptRngOffsetIndex:X02})";
         _scriptRngOffsetLabel.Text = $"{scriptRngOffset} ({scriptRngOffset:X02})";
+
+        // because gameplay runs at 30fps, don't show a 0 value unless it happens two frames in a row
+        if (_randCalls.FrameCalls == 0 && !_framerateToggle) {
+            _framerateToggle = true;
+        } else {
+            _rngFrameCallsLabel.Text = $"{_randCalls.FrameCalls}";
+            _framerateToggle = false;
+        }
+
+        _rngRoomCallsLabel.Text = $"{_randCalls.RoomCalls}";
+        _rngTotalCallsLabel.Text = $"{_randCalls.TotalCalls}";
     }
 }
